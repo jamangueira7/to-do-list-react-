3dce62c9779202b61ea61ccce97c893dfbc90ae7
João Mangueira <jamjs7@gmail.com>
2021-10-18 20:44:42 -0300

    add mais estilização e o primeiro model

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import {useState } from 'react';
 import * as Components from './App.styles';
 
 const App = () => {
+    const [list, setList] = useState([]);
+
     return(
         <Components.Container>
-            ...
+            <Components.Area>
+                <Components.Header>Lista de Tarefas</Components.Header>
+            </Components.Area>
         </Components.Container>
     );
 }
