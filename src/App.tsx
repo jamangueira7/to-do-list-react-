b36fb813b777e5be5b9c08b46ce8b26e36dffbe3
João Mangueira <jamjs7@gmail.com>
2021-10-18 20:47:39 -0300

    model criado e state criado

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import {useState } from 'react';
 import * as Components from './App.styles';
+import { Item } from './types/Item';
 
 const App = () => {
-    const [list, setList] = useState([]);
+    const [list, setList] = useState<Item[]>([
+        {id: 1, name: 'Compra o pão na padaria', done: false},
+        {id: 2, name: 'Compra um bolo na padaria', done: false}
+    ]);
 
     return(
         <Components.Container>
             <Components.Area>
                 <Components.Header>Lista de Tarefas</Components.Header>
+
+                
             </Components.Area>
         </Components.Container>
     );
